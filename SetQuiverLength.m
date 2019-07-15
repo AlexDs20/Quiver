ce82badfaf19eeb841e0ad4b2ae351f7b8a5572a
Alexandre <alexandre.de.spiegeleer@umu.se>
2019-07-15 11:44:40 +0200

    Added optional parameter: HeadLength. The input is given as a pair: ('HeadLength',value)
    Giving this optional parameter a value will set all the
    vectors to have the set length for the Head.
    It is in the units of the xyz axis.

--- SetQuiverLength.m
+++ SetQuiverLength.m
@@ -1,4 +1,4 @@
-function SetQuiverLength(q,mags)
+function SetQuiverLength(q,mags,varargin)
 %--------------------------------------------------
 % function SetQuiverLength(q,mags)
 %
@@ -8,6 +8,9 @@ function SetQuiverLength(q,mags)
 % Input: q    = handle to quiver plot, can be quiver or quiver3
 %        mags = Desired length of each vector in units of the x,y axis
 %               If mags is a scalar, all the vector will have that length
+% Optional inputs: given as ('variable',value)
+%        'HeadLength' = single value for the length of the head in units of the xyz axis.
+%                       It is applied to all the vectors
 %
 % NOTE: For some unknown reason, MATLAB does not always simply change the length of the vectors and requires a
 %       pause statement towards the end.
@@ -22,12 +25,20 @@ function SetQuiverLength(q,mags)
 %     drawnow;                  % This is needed as, if the plot is not plotted, the VertexData for the quiver are not existent.
 %     view(180,0);
 %     mag = 0.1*ones(size(u));  % Length of the vectors : 0.1
-%     SetQuiverLength(q,mag);
+%     SetQuiverLength(q,mag,'HeadLength',0.05);
 %
 %--------------------------------------------------
 %   Authorship:
 %     Function made by Alexandre De Spiegeleer. Feel free to use it.
 %--------------------------------------------------
+
+%// Set default values of varargin
+HeadLength = [];
+
+%// Read the optional inputs
+if find(strcmp('HeadLength',varargin))
+  HeadLength = varargin{ find(strcmp('HeadLength',varargin))+1 };
+end
 
 %// Start by removing the autoscale option
 set(q,'AutoScale','Off');
@@ -85,8 +96,12 @@ Head_dir_a = diff(Head_ori(:,[2,1],:),1,2)./permute(Head_length(:,:,ones(size(H.
 Head_dir_b = diff(Head_ori(:,[2,3],:),1,2)./permute(Head_length(:,:,ones(size(H.VertexData,1),1)),[3 2 1]);
 
 %// New length of head
-Head_mag = mags.*Length_ratio;
-Head_mag = permute(Head_mag(:,:,ones(size(H.VertexData,1),1)),[3 2 1]);
+if isempty(HeadLength)
+  Head_mag = mags.*Length_ratio;
+  Head_mag = permute(Head_mag(:,:,ones(size(H.VertexData,1),1)),[3 2 1]);
+elseif ~isempty(HeadLength)
+  Head_mag = repmat(HeadLength,[size(H.VertexData,1) 1 size(Head_ori,3)]);
+end
 
 %// New Tail End = start of tail + magnitude*normalized direction
 TailVertex = Tail_ori;
